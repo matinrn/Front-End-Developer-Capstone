4011c46ab35aeb1cfc9ddcf7da9a70f26a51d33f
Matin Rahmati Nezhad <matin.mrn11@gmail.com>
2024-05-11 17:15:53 +0330

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,14 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 import { Reservations } from './components';
+import { BrowserRouter } from 'react-router-dom';
 
 test('Checking Time Status Section', () => {
-  render( <Reservations />);
+  render( 
+    <BrowserRouter>
+  <Reservations />
+    </BrowserRouter>
+);
 
   const statusHeadaing = screen.getByText("Evening");
 
